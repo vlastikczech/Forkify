150d580a9bc7901f7445c8d09ca6790556d59e2e
vlastik <vlastikczech1@gmail.com>
2018-06-02 16:32:31 -0700

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const NodemonPlugin = require( 'nodemon-webpack-plugin' )
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'), // path.resolve joins the current absolute path with where we want the bundle to be in
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'), // path.resolve joins the current absolute path with where we want the bundle to be in
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [new NodemonPlugin()],
+    mode: "none"
 };
