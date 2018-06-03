3563b6b309d2ebdf4c230839da6ce0d87c7bd83a
vlastik <vlastikczech1@gmail.com>
2018-06-02 17:05:06 -0700

    Added babel, babel-loader, babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const NodemonPlugin = require( 'nodemon-webpack-plugin' )
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'), // path.resolve joins the current absolute path with where we want the bundle to be in
         filename: 'js/bundle.js'
@@ -10,6 +11,19 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    plugins: [new NodemonPlugin()],
+    plugins: [new NodemonPlugin(),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.js$/,  //looks for all the files and test if the files end in .js
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
     mode: "none"
 };
