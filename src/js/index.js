3d609772fbf16b93b35be5befcab67ce82113fad
vlastik <vlastikczech1@gmail.com>
2018-06-03 16:49:27 -0700

    Made first api calls in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '1bf5bc759dc4250812edf03e08bfb8c4';
+    try {
+         const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+    } catch (error){
+        alert(error);
+    }
+   
+}
+getResults();
+
+//fetch doesnt support all browsers
+// 1bf5bc759dc4250812edf03e08bfb8c4
+// http://food2fork.com/api/search
