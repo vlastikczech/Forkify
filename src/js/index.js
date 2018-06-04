89f761ba22f589268c4e7dcd1970ede48cd0e154
vlastik <vlastikczech1@gmail.com>
2018-06-03 18:03:27 -0700

    Started building the Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
 import Search from './models/Search';
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shhopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-const search = new Search('pizza');
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //TODO
+
+    if (query) {
+        // 2) New search object and add it to state
+        state.search = new Search(query)
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults(); //awaits for the results and comes back with data
+
+        // 5) render results on UI
+        console.log(state.search.result);
+    }
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 search.getResults();
+
