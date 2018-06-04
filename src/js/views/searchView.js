8cdb123504dbacce0765775124e00c30f03a160a
vlastik <vlastikczech1@gmail.com>
2018-06-04 10:46:45 -0700

    Built second part of the Search View

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,21 @@ export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((accumlator, current) => {
+            if ( accumlator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumlator + current.length;
+        }, 0)
+
+        return `${newTitle}.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -18,7 +33,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
