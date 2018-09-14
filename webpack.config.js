09955a53e08c96c419987f4bce9a62e542eff26a
vlastik <vlastikczech1@gmail.com>
2018-09-14 15:12:32 -0700

    fixed port

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     devServer: {
         contentBase: './dist',
-        port: 'process.env.PORT || 5080'
+        port: process.env.PORT || 5080
     },
     plugins: [
         new HtmlWebpackPlugin({
